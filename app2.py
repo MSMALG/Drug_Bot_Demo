7008427da88b8987efdbe3f97ea2c427c9f44341
Muzna <91617542+MSMALG@users.noreply.github.com>
2024-11-16 12:03:59 +0300

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,13 +1,13 @@
 import pandas as pd
 import streamlit as st
 
-file_path = 'drug_nomenclature_and_modifications.xlsx'  # Update with your file path
+file_path = 'drug_nomenclature_and_modifications.xlsx'  
 df = pd.read_excel(file_path)
 
-# Function to fetch molecule image from PubChem
+#Function to fetch molecule image from PubChem
 def fetch_pubchem_image(smiles, width=400, height=400):
     base_url = "https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/smiles/png"
-    # Create the request URL
+    #Create the request URL
     url = f"{base_url}?smiles={smiles}&width={width}&height={height}"
     return url
 
@@ -24,7 +24,7 @@ if target_receptor:
         
         st.subheader(f"Recommended Drug: {drug_name}")
         
-        # Display the molecular structure using PubChem
+        #Display the molecular structure 
         if smiles:
             pubchem_url = fetch_pubchem_image(smiles)
             st.image(pubchem_url, caption=f"Molecular Structure of {drug_name}")
@@ -40,7 +40,7 @@ if target_receptor:
                 st.subheader(f"Side Effect: {side_effect}")
                 st.text(f"Solution: {modification}")
                 
-                # Display the modified molecular structure using PubChem
+                #Display the modified molecular structure 
                 if mod_smiles:
                     pubchem_mod_url = fetch_pubchem_image(mod_smiles)
                     st.image(pubchem_mod_url, caption="Modified Molecular Structure")
